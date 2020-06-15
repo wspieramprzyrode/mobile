0f3bb17e10679841d7a2444255848d6cbd5aaf7f
Lukasz Gawron <lgawron@thesoftwarehouse.pl>
2020-06-16 00:15:38 +0200

    refactor InventoryService.getCategories method

--- lib/services/api/inventory_service.dart
+++ lib/services/api/inventory_service.dart
@@ -6,11 +6,14 @@ import 'package:dio/dio.dart';
 import 'package:wspieramprzyrode/services/api/client.dart';
 
 class InventoryService {
-  Dio dio = ApiClient('https://api-dev.wspieramprzyrode.pl/v1/inventory').getApiClient();
+  Dio dio = ApiClient('https://api-dev.wspieramprzyrode.pl/v1/inventory')
+      .getApiClient();
 
   Future<List<InventoryObject>> getObjects() async {
-    print('GET $endpoint');
-    final response = await dio.get('/objects', options: Options(responseType: ResponseType.plain));
+    final response = await dio.get(
+      '/objects',
+      options: Options(responseType: ResponseType.plain),
+    );
     if (response.statusCode == 200) {
       try {
         final decodedResponse = json.decode(response.data) as List;
@@ -18,28 +21,29 @@ class InventoryService {
             .map((obj) => InventoryObject.fromJson(obj))
             .toList();
       } catch (err) {
-        print('GET $endpoint failed to parse response - ${Error.safeToString(err)}');
         return [];
       }
     } else {
-      print('GET $endpoint failed with status: ${response.statusCode}.');
       return [];
     }
   }
 
   Future<List<InventoryCategory>> getCategories() async {
-    // TODO: remove it
-    print("request to api");
-    final response = await dio.get('/categories', options: Options(responseType: ResponseType.plain));
+    final response = await dio.get(
+      '/categories',
+      options: Options(responseType: ResponseType.plain),
+    );
     if (response.statusCode == 200) {
-      var parsedResponse = json.decode(response.data);
-      List<InventoryCategory> objects = List<InventoryCategory>();
-      parsedResponse.forEach((object) {
-        objects.add(InventoryCategory.fromJson(object));
-      });
-      return objects;
+      try {
+        final decodedResponse = json.decode(response.data) as List;
+        return decodedResponse
+            .map((obj) => InventoryCategory.fromJson(obj))
+            .toList();
+      } catch (err) {
+        return [];
+      }
     } else {
-      print('Request failed with status: ${response.statusCode}.');
+      return [];
     }
   }
 }
